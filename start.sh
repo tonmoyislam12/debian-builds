40647361a780d80773e2fe3fb23c84cc64954090
Tonmoy <127471199+tonmoyislam12@users.noreply.github.com>
2023-03-17 23:07:47 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,8 @@
-
 sudo apt-get install sbuild schroot debootstrap apt-cacher-ng devscripts piuparts
-cp .sbuildrc ~/.sbuildrc
+cp -v .sbuildrc ~/.sbuildrc
 echo $USER
-echo $LOGNAME
 whoami
-sudo sbuild-adduser $LOGNAME
-whoami
+sudo sbuild-adduser $USER
 sudo ln -sf ~/.sbuildrc /root/.sbuildrc
 newgrp sbuild
 sudo sbuild-createchroot --include=eatmydata,ccache unstable /srv/chroot/unstable-amd64-sbuild http://ftp.us.debian.org/debian
