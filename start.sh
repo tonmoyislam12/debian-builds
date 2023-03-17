ba97a557b48ec64df7ec73d3c2b487a4ba364135
Tonmoy <127471199+tonmoyislam12@users.noreply.github.com>
2023-03-17 23:17:25 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,4 +8,4 @@ newgrp sbuild
 sudo sbuild-createchroot --include=eatmydata,ccache unstable /srv/chroot/unstable-amd64-sbuild http://ftp.us.debian.org/debian
 sudo sbuild-update -udcar u
 pwd && mkdir -pv myapp && cd myapp
-cat /etc/apt/source.list
+cat /etc/apt/sources.list
