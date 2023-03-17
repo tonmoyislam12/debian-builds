18355ebb7d2b5c4f606864c9ddbda607b7d06d61
Tonmoy <127471199+tonmoyislam12@users.noreply.github.com>
2023-03-17 22:56:38 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,6 @@
-sudo apt-get install sbuild schroot debootstrap apt-cacher-ng devscripts piuparts
-sudo tee ~/.sbuildrc << EOF
-$distribution = 'unstable';
-$build_arch_all = 1;
-$build_source = 1;
-$source_only_changes = 1;
-$verbose = 1;
-$ENV{'DEB_BUILD_OPTIONS'} = 'parallel=5';
-$run_lintian = 1;
-$lintian_opts = ['-i', '-I'];
-$run_piuparts = 1;
-$piuparts_opts = ['--schroot', '%r-%a-sbuild', '--no-eatmydata'];
-$run_autopkgtest = 1;
-$autopkgtest_root_args = '';
-$autopkgtest_opts = [ '--', 'schroot', '%r-%a-sbuild' ];
-EOF
 echo $LOGNAME
+sudo apt-get install sbuild schroot debootstrap apt-cacher-ng devscripts piuparts
+cp ./sbuildrc ~/.sbuildrc
 sudo sbuild-adduser $LOGNAME
 sudo ln -sf ~/.sbuildrc /root/.sbuildrc
 newgrp sbuild
