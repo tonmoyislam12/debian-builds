7fbcf277f71c11bb04b813bb4ae8465de2e3f915
Tonmoy <127471199+tonmoyislam12@users.noreply.github.com>
2023-03-17 23:43:09 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,4 +11,10 @@ newgrp sbuild
 sudo sbuild-createchroot --include=eatmydata,ccache unstable /srv/chroot/unstable-amd64-sbuild http://ftp.us.debian.org/debian
 sudo sbuild-update -udcar u
 cat /etc/apt/sources.list
-pwd && mkdir -pv myapp && cd myapp && sudo apt source aria2 qbittorrent-nox ffmpeg && ls -la .
+pwd && mkdir -pv aria2 && cd aria2 && sudo apt source aria2
+pwd && mkdir -pv qbit && cd qbit && sudo apt source qbittorrent-nox
+pwd && mkdir -pv mpeg && cd mpeg && sudo apt source ffmpeg
+ls -a aria2
+ls -a qbit
+ls -a mpeg
+cd aria2/aria2-1.36.0/ && sudo sbuild
